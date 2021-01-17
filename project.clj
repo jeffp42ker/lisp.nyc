4c9739a819db782f1a16c541a0c12c3c354c3de1
Jeff Parker <jeffp42ker@gmail.com>
2021-01-17 12:26:15 -0500

    add clojure.tools.cli dependency

--- project.clj
+++ project.clj
@@ -8,9 +8,10 @@
                            [compojure "1.6.2"]
                            [ring-server "0.5.0"]
                            [cryogen-flexmark "0.1.4"]
+                           [org.clojure/tools.cli "1.0.194"]
                            [cryogen-core "0.4.0"]]
             :plugins [[lein-ring "0.12.5"]]
-            :main cryogen.core
+  :main cryogen.core
             :ring {:init cryogen.server/init
                    :handler cryogen.server/handler}
             :aliases {"serve"      ["run" "-m" "cryogen.server"]
